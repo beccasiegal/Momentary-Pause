d7b2067dd01c875e5407bfaf0a6f57171c86dbb9
Rebecca <becca.siegal@gmail.com>
2020-03-23 13:06:49 -0400

    Updated index.html

---
+++ data-store.js
@@ -0,0 +1,11 @@
+"https://www.themomentarypause.com/galleries/south-rim-grand-canyon/"
+"https://www.themomentarypause.com/galleries/autumns-majesty/" 
+"https://www.themomentarypause.com/galleries/scenes-from-country-roads/"
+"https://www.themomentarypause.com/galleries/beavers-2/"
+"https://www.themomentarypause.com/galleries/beavers-of-new-york-2/">
+<a href="https://www.themomentarypause.com/galleries/galapagos-islands-3/">GALAPAGOS ISLANDS</a>
+<a href="https://www.themomentarypause.com/galleries/alaska/">ALASKA</a>
+<a href="https://www.themomentarypause.com/galleries/sunrise-sunset/">SUNRISE, SUNSET</a>
+<a href="https://www.themomentarypause.com/galleries/pelicans/">PELICANS</a>
+<a href="https://www.themomentarypause.com/galleries/osprey-and-eagles/">OSPREY AND EAGLES</a>
+<a href="https://www.themomentarypause.com/galleries/bob-and-friends/"
